a7da702807dfbc48a9ffcff59a605a2c836eff2b
Roberto Paz <fsjedi@outlook.com>
2021-10-25 11:14:13 -0400

    tercer commit

--- app.js
+++ app.js
@@ -3,10 +3,30 @@ const menu = {
       appetizers: [],
       mains: [],
       desserts: []
-    }
+    },
     get appetizers() {
-  
+    },
+    set appetizers(appetizerIn) {
+
     },
     
+    get mains() {
+    },
+    set mains(mainIn) {
+
+    },
+
+    get desserts() {
+    },
+    set desserts(dessertIn) {
+
+    },
+
+    get courses () {
+      return {
+        appetizers: this.appetizers,
+        mains: this.mains,
+        desserts: this.desserts
+      }
+    }
   }
-  
